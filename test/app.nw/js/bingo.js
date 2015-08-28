0d6979b2c1a6227c6eece3932031c576a47ceb9e
CQzhao <cqzhao_ne@hotmail.com>
2015-08-28 11:19:20 +0800

    Adding Bingo, need to write checkwin function

--- test/app.nw/js/bingo.js
+++ test/app.nw/js/bingo.js
@@ -1,17 +1,58 @@
 window.onload = init;
+var usedNum = [];
 
 function init () {
+	if (document.getElementById){
+		document.getElementById("reload").onclick = anotherCard;
+		newCard();
+	}else{
+		alert("Ur browser need javascript!");
+	}
+}
+
+function newCard(){
 	for (var i = 1; i <= 25; i++) {
 		setSquare(i);
 	}
 }
 
+function anotherCard () {
+	for (var i = 0; i < usedNum.length; i++) {
+		usedNum[i] = false;
+	}; 
+	newCard();
+}
+
 function setSquare (i) {
 	var current = document.getElementById("square"+i);
 	if(current){
-		current.innerHTML = getNum(i);
-		console.log(current+"  "+getNum(i));
+		var newNum = null;
+		do{
+			newNum = getNum(i);
+		}while(usedNum[newNum]);
+		current.innerHTML = newNum;
+		current.className = "";
+		current.onmousedown = picked;
+		usedNum[newNum] = true;
 	}
+}
+
+function picked (event) {
+	if(event){
+		var thisSquare = event.target;
+	}else{
+		var thisSquare = window.event.srcElement;
+	}
+	if(thisSquare.className === ""){
+		thisSquare.className = "pickedBG";
+	}else{
+		thisSquare.className = "";
+	}
+	checkWin();
+}
+
+function checkWin () {
+	
 }
 
 function getNum (i) {
